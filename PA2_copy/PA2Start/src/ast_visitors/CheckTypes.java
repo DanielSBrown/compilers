ece1489c91e6c0b022d775d069c84d19a230c600
DanielSBrown <dsb9ef@virginia.edu>
2017-10-25 21:21:00 -0400

    Please Work

--- PA2_copy/PA2Start/src/ast_visitors/CheckTypes.java
+++ PA2_copy/PA2Start/src/ast_visitors/CheckTypes.java
@@ -88,8 +88,7 @@ Start PA2 Grammar Typechecking
        throw new SemanticException(
               "Operands for MeggySetPixel must be BYTE, BYTE, COLOR",
                     node.getXExp().getLine(),
-                    node.getYExp().getPos(),
-                    node.getColor().getLine());
+                    node.getXExp().getPos());
      }
 
    }
@@ -102,7 +101,8 @@ Start PA2 Grammar Typechecking
      else {
        throw new SemanticException(
               "Operands for ByteCast must be INT",
-                    node.getExp().getLine());
+                    node.getExp().getLine(),
+                    node.getExp().getPos());
      }
    }
 
@@ -117,39 +117,43 @@ Start PA3 Grammar Typechecking
       else {
         throw new SemanticException(
                "Operands for MeggyDelay must be BOOL",
-                     node.getExp().getLine());
+                     node.getExp().getLine(),
+                     node.getExp().getPos());
       }
     }
 
     public void outIfStatement(IfStatement node) { //CHECK THIS
       Type expType = this.mCurrentST.getExpType(node.getExp());
-      if (!expType==Type.BOOL) {
+      if (expType!=Type.BOOL) {
         throw new SemanticException(
-               "Operands for MeggyDelay must be BOOL",
-                     node.getExp().getLine());
+               "Operands for IfStatement must be BOOL",
+                     node.getExp().getLine(),
+                     node.getExp().getPos());
                    }
-      this.mCurrentST.setExpType(node, Type.BOOL);
+    //  this.mCurrentST.setExpType(node, Type.BOOL);
     }
 
     public void outWhileStatement(WhileStatement node) {  //Here too
       Type expType = this.mCurrentST.getExpType(node.getExp());
-      if (!expType==Type.BOOL) {
+      if (expType==Type.BOOL) {
         throw new SemanticException(
                "Operands for MeggyDelay must be BOOL",
-                     node.getExp().getLine());      }
+                     node.getExp().getLine(),
+                     node.getExp().getPos());      }
       this.mCurrentST.setExpType(node, Type.BOOL);
     }
 
     public void outEqualExp(EqualExp node) {   //HERE too....
       Type lexpType = this.mCurrentST.getExpType(node.getLExp());
       Type rexpType = this.mCurrentST.getExpType(node.getRExp());
-      if (lexpType==Type.BOOL && rexpType==Type.BOOL) {
+      if ((lexpType==Type.BOOL && rexpType==Type.BOOL) || ((lexpType==Type.BYTE || lexpType==Type.INT) && (rexpType==Type.BYTE || rexpType==Type.INT))){
         this.mCurrentST.setExpType(node, Type.BOOL);
       }
       else {
         throw new SemanticException(
-               "Operands for equals operation must be BOOL",
-                     node.getExp().getLine());
+               "Operands for equals operation must be BOOL, INT, or BYTE",
+                     node.getLExp().getLine(),
+                     node.getLExp().getPos());
       }
     }
 
@@ -189,13 +193,14 @@ Start PA3 Grammar Typechecking
 
     public void outNegExp(NegExp node) {
       Type expType = this.mCurrentST.getExpType(node.getExp());
-      if (expType==Type.INT || expType==Type.BOOL) {
+      if (expType==Type.INT || expType==Type.BYTE) {
         this.mCurrentST.setExpType(node, Type.INT);
       }
       else {
         throw new SemanticException(
                "Operands for Negation must be INT or BOOL",
-                     node.getExp().getLine());
+                     node.getExp().getLine(),
+                     node.getExp().getPos());
       }
     }
 
@@ -209,7 +214,8 @@ Start PA3 Grammar Typechecking
       else {
         throw new SemanticException(
                "Operands for MeggyGetPixel must be BYTE",
-                     node.getExp().getLine());
+                     node.getXExp().getLine(),
+                     node.getXExp().getPos());
       }
     }
 
@@ -222,7 +228,8 @@ Start PA3 Grammar Typechecking
       else {
         throw new SemanticException(
                "Operands for MeggyCheckButton must be BUTTON",
-                     node.getExp().getLine());
+                     node.getExp().getLine(),
+                     node.getExp().getPos());
       }
     }
 
@@ -234,8 +241,29 @@ Start PA3 Grammar Typechecking
       else {
         throw new SemanticException(
                "Operands for '!' must be BYTE",
-                     node.getExp().getLine());
+                     node.getExp().getLine(),
+                     node.getExp().getPos());
       }
     }
+
+    public void outProgram(Program node) {
+
+    }
+    public void outMainClass(MainClass node) {
+
+    }
+    public void outBlockStatement(BlockStatement node) {
+
+    }
+    public void outButtonExp(ButtonLiteral node) {
+
+    }
+    public void outColorExp(ColorLiteral node) {
+
+    }
+    public void outIntegerExp(IntLiteral node) {
+
+    }
+
 
 }
