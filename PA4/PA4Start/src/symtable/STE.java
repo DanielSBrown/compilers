520503a2c46bebcc4dfdcbeb3f254a33cdbcf715
Chase Deets <chd5hq@virginia.edu>
2017-11-20 17:13:53 -0500

    STE made

--- PA4/PA4Start/src/symtable/STE.java
+++ PA4/PA4Start/src/symtable/STE.java
@@ -6,8 +6,21 @@ import exceptions.InternalException;
 
 public Class STE{
 
+  private String mName;
+
+  public STE(String id){
+    mName = id;
+  }
+
+  public String getName(){
+      return mName;
+  }
+
+  public Void setName(String id){
+    nName = id;
+  }
 
 
 
-  
+
 }
--- PA4/PA4Start/src/symtable/Scope.java
+++ PA4/PA4Start/src/symtable/Scope.java
@@ -6,8 +6,8 @@ import exceptions.InternalException;
 
 public Class Scope{
 
-
-
+  //Figuring out the rest of what happens here from the lecture slides
+  
 
 
 }
